ce08003744b00a93b93da610838977442d055a11
Dominik Schmid <DominikSchmid93@gmail.com>
2017-11-17 19:01:50 +1100

    Cleaned up the greedy algorithm

--- algorithms/greedy.py
+++ algorithms/greedy.py
@@ -1,5 +1,4 @@
 from game_logic import merge, all_directions, score
-import random
 
 """
 This algorithm tests which direction results in the highest score and chooses greedily.
@@ -9,10 +8,11 @@ def get_next_move(matrix):
     """Return one of the dir_ constants from game_logic"""
 
     def get_value(mat):
-        # tuple ordering prefers earlier values
+        # Tuple ordering prefers earlier values.
+        # Here we prefer new matricies, then higher-score matricies.
         return (mat != matrix, score(mat))
 
-    best_score, best_direction = max(
+    best_value, best_direction = max(
         (get_value(merge(matrix, direction)), direction)
         for direction in all_directions
     )
