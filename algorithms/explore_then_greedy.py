7c4233fb24c7bbcdff63a7886ae0651de9623829
Dominik Schmid <DominikSchmid93@gmail.com>
2017-11-24 23:07:16 +1100

    Made explore sampling better.

--- algorithms/explore_then_greedy.py
+++ algorithms/explore_then_greedy.py
@@ -6,27 +6,10 @@ import random
 random.seed(4)
 
 """
-Tries to avoid paths that lead to constricting options.
+minimax by distributing samples of where to explore.
 """
 
-# dir_tiebreaker = {
-#     dir_right: 4,
-#     dir_down: 3,
-#     dir_left: 2,
-#     dir_up: 1,
-# }
-
 samples_remaining = None  # initialized in get_next_move
-
-# def even_distribute(num_balls, num_boxes):
-#     base_amount, uneven_amount = divmod(num_balls, num_boxes)
-#     boxes = [
-#         base_amount+1 if i<uneven_amount else base_amount
-#         for i in range(num_boxes)
-#     ]
-#     random.shuffle(boxes)
-#     assert sum(boxes) == num_balls
-#     return boxes
 
 # Puts balls into buckets such that the distribution looks like
 # the distribution of the weights
@@ -46,11 +29,11 @@ def distribute(num_balls, weights):
 def my_score(matrix):
     return len(find_empty_positions(matrix))*10000 + score(matrix)
 
-def fills_and_num_samples(mat, max_samples):
+def get_fills_and_num_samples(mat, max_samples):
     '''
-    >>> list(fills_and_num_samples( ((0,4,4,4),(4,4,4,4),(4,4,4,4),(4,4,4,4)), 100))
+    >>> list(get_fills_and_num_samples( ((0,4,4,4),(4,4,4,4),(4,4,4,4),(4,4,4,4)), 100))
     [(((0, 0), 4), 10), (((0, 0), 2), 90)]
-    >>> list(fills_and_num_samples( ((0,4,4,4),(4,4,0,4),(4,4,4,4),(4,4,4,4)), 200))
+    >>> list(get_fills_and_num_samples( ((0,4,4,4),(4,4,0,4),(4,4,4,4),(4,4,4,4)), 200))
     [(((1, 2), 4), 10), (((1, 2), 2), 90), (((0, 0), 2), 90), (((0, 0), 4), 10)]
     '''
     empty_positions = find_empty_positions(mat)
@@ -78,48 +61,67 @@ def directions_and_samples(matrix, max_samples):
     num_samples = distribute(max_samples, weights)
     return zip(dirs, num_samples)
 
-def combine_dir_scores(scores):
-    return max(scores)
 def combine_fill_scores(scores):
     average = sum(scores) / float(len(scores))
     return min(scores) * 0.99 + average * 0.01
 
 
-def get_value_of_move(matrix, direction, max_samples=200):
-
-    mat = merge(matrix, direction)
-    if mat == matrix:
-        return 0
-
+def get_value_of_post_move(mat, max_samples):
     if max_samples <= 1:
-        return my_score(matrix)
-    del matrix  # avoid mistakes
+        return my_score(mat)
+
+    fills_and_num_samples = get_fills_and_num_samples(mat, max_samples)
+    if not fills_and_num_samples:
+        # return 0
+        return my_score(mat)
 
     fill_scores = []
     sample_count = 0
-    for fill, fill_samples in fills_and_num_samples(mat, max_samples):
+    for fill, fill_samples in fills_and_num_samples:
         if not fill_samples:
             continue
         # do fill, recurse on filled matrix.
         filled_mat = set_value(mat, fill[0], fill[1])
-        dir_scores = []
-        for new_dir, dir_samples in directions_and_samples(filled_mat, fill_samples):
-            new_value = get_value_of_move(filled_mat, new_dir, dir_samples)
-            dir_scores.append(new_value)
-            sample_count += dir_samples
-        fill_scores.append(combine_dir_scores(dir_scores))
+        fill_scores.append(get_value_and_move(filled_mat, fill_samples)[0])
+        sample_count += fill_samples
     assert sample_count == max_samples
+
     return combine_fill_scores(fill_scores)
     # return my_score(mat)
 
+def get_explore_weights(merged_matrices):
+    return [1] * len(merged_matrices)
+
+
+def get_value_and_move(starting_matrix, max_samples=1000):
+
+    if max_samples <= 1:
+        return (my_score(starting_matrix), dir_down)
+
+
+    merged_states = [ (merge(starting_matrix, direction), direction) for direction in all_directions ]
+    valid_states = [ (mat, direction) for (mat, direction) in merged_states if mat != starting_matrix]
+    weights = get_explore_weights([ mat for mat,direction in valid_states ])
+    num_samples_per_state = distribute(max_samples, weights)
+
+    if not valid_states:
+        # return 0
+        return (my_score(starting_matrix), dir_down)
+
+    dir_scores = []
+    sample_count = 0
+    for (mat, direction), num_samples in zip(valid_states, num_samples_per_state):
+        if not num_samples:
+            continue
+        new_value = get_value_of_post_move(mat, num_samples)
+        dir_scores.append(new_value)
+        sample_count += num_samples
+    rankables = [ (score, direction) for score, (mat, direction) in zip(dir_scores, valid_states) ]
+    return max(rankables)
 
 def get_next_move(starting_matrix):
     """Return one of the dir_ constants from game_logic"""
 
-    best_value, best_direction = max(
-        (get_value_of_move(starting_matrix, direction), direction)
-        for direction in all_directions
-    )
-
+    best_value, best_direction = get_value_and_move(starting_matrix)
 
     return best_direction
